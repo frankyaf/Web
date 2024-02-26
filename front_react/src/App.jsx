9f2caf23ac7dccbd6097fbf1f6d194470ea949a6
franky <ar.alonso.fuentes@gmail.com>
2024-02-25 23:27:35 -0600

    Primer Vistazo React

---
+++ front_react/src/App.jsx
@@ -0,0 +1,7 @@
+import { TwitchCard } from './TwitchFollowCard'
+import './TwitchCard.css'
+export function App (){
+    return(
+     <TwitchCard/>
+    )
+}
---
+++ front_react/src/TwitchFollowCard.jsx
@@ -0,0 +1,15 @@
+export function TwitchCard (){
+    return(
+        <article className='tw-card'>
+            <header>
+                <img src="https://unavatar.io/python" alt="Avatar de Python" srcset="" />
+                <div>Python</div>
+                <span>@PythonBest</span>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
