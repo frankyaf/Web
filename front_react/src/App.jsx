37b1877f865edaac83d84698791b9dda17f7eb56
franky <ar.alonso.fuentes@gmail.com>
2024-02-28 18:44:06 -0600

    TwitchCard

--- front_react/src/App.jsx
+++ front_react/src/App.jsx
@@ -1,7 +1,23 @@
 import { TwitchCard } from './TwitchFollowCard'
 import './TwitchCard.css'
 export function App (){
+    const format = (userName) => `@${userName}`
     return(
-     <TwitchCard/>
+     <section className='App'>
+     <TwitchCard 
+        formatUserName={format} 
+        isFollowing 
+        userName="PythonBest" 
+        name="Python" />
+     <TwitchCard
+        formatUserName={format} 
+        isFollowing={false} 
+        userName="PHPOld" 
+        name="PHP"/>
+     <TwitchCard 
+        formatUserName={format}
+        userName="ReactFront" 
+        name="React"/>
+     </section>
     )
 }
--- front_react/src/TwitchFollowCard.jsx
+++ front_react/src/TwitchFollowCard.jsx
@@ -1,15 +1,23 @@
-export function TwitchCard (){
+export function TwitchCard ({formatUserName,userName,name, isFollowing}){
+    console.log(isFollowing);
     return(
         <article className='tw-card'>
-            <header>
-                <img src="https://unavatar.io/python" alt="Avatar de Python" srcset="" />
-                <div>Python</div>
-                <span>@PythonBest</span>
+            <header className='tw-card-header'>
+                <img 
+                className='tw-card-image'
+                src={`https://unavatar.io/${name}`} alt="Avatar de Python"/>
+                <div className='tw-card-info'>
+                    <strong>{name}</strong>
+                    <span>{formatUserName(userName)}</span>
+                </div>
             </header>
 
             <aside>
-                <button>Seguir</button>
+                <button className='tw-card-button'>
+                    Seguir
+                </button>
             </aside>
         </article>
     )
 }
+
